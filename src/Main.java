21b5afb82ac757bc7312cfeac7e46eaf7139dbe2
Daniel T. Avila <danieltavila@gmail.com>
2020-08-25 14:46:15 -0300

    Template Basico

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,24 @@ public class Main {
     public static void main(String[] args) {
 
         Catalogo MeuCatalogoA;
-
         Veiculo MeuVeiculo;
 
-        MeuVeiculo = new Veiculo("Toyota", "Yarris", 1000, 10);
+        System.out.println("Inicio");
 
         MeuCatalogoA = new Catalogo(10);
 
+        MeuVeiculo = new Veiculo("Toyota", "Yarris", 1000, 10);
         MeuCatalogoA.setVeiculo(MeuVeiculo);
 
-        System.out.print("Fim");
+        MeuVeiculo = new Veiculo("Toyota", "Corolla", 10000, 100);
+        MeuCatalogoA.setVeiculo(MeuVeiculo);
+
+        MeuVeiculo = new Veiculo("Toyota", "Hylux", 2000, 20);
+        MeuCatalogoA.setVeiculo(MeuVeiculo);
+
+        System.out.print(MeuCatalogoA.DescCatalogo());
+
+        System.out.println("Fim");
 
     }
 }
